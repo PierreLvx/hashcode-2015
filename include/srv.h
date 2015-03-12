dbe736438c2f21bc4278bd73e6043afcef800dc2
Cedric LEGRAND <celegran@student.42.fr>
2015-03-12 19:19:20 +0100

    master

---
+++ include/srv.h
@@ -0,0 +1,17 @@
+#ifndef		SRV_H
+# define	SRV_H
+
+struct data
+{
+	int		rows;
+	int		cols;
+	int		unavail;
+	int		grps;
+	int		srvs;
+};
+
+struct data	get_data(int);
+int**		make_tab(struct data*);
+void		init_racks(int**, int);
+
+#endif
