399303249e5257be10fe8d7e70bcf555ed9a4d24
Pierre Lavaux <pierre.lvx@gmail.com>
2015-03-12 19:41:12 +0100

    menage

--- include/srv.h
+++ include/srv.h
@@ -2,6 +2,7 @@
 # define	SRV_H
 
 # include <stdio.h>
+# include <stdlib.h>
 
 struct data
 {
--- src/serveur.c
+++ src/serveur.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "srv.h"
 
 int		**make_tab(struct data *d)
