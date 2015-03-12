cfb984a541c2d5b58f228ad8e4e3af99424f1a69
Cedric LEGRAND <celegran@student.42.fr>
2015-03-12 20:12:40 +0100

    fixed racks parsing

--- src/init_racks.c
+++ src/init_racks.c
@@ -7,6 +7,7 @@ void	init_racks(int **srv_rack, struct data *srv_data, FILE *fd)
 	for (i = 0; i < srv_data->rows; i++)
 		for (j = 0; j < srv_data->cols; j++)
 			srv_rack[i][j] = 0;
+	i = 0;
 	while (i < srv_data->unavail)
 	{
 		fscanf(fd, "%i %i\n", &x, &y);
