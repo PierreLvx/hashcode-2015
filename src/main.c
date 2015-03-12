2d62d8221e90d644162cd7386d01f137845d88a0
Cedric LEGRAND <celegran@student.42.fr>
2015-03-12 19:22:15 +0100

    main

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int		main(int argc, char **argv)
+{
+	struct data	srv_data;
+	FILE		*fd;
+	int			**srv_rack;
+
+	if (argc < 2)
+		return -1;
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+		return -2;
+	srv_data = get_data(fd);
+	srv_rack = make_tab(&srv_data);
+	init_racks(srv_rack, fd);
+	return 0;
+}
