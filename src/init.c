5a6af81ff6dce2e955c782922c3f938ffc773359
Cedric LEGRAND <celegran@student.42.fr>
2015-03-12 19:29:43 +0100

    parse datas

---
+++ src/init.c
@@ -0,0 +1,17 @@
+#include "srv.h"
+
+struct data	get_data(FILE* fd)
+{
+	struct data srv_data;
+
+	fscanf(
+		fd,
+		"%i %i %i %i %i\n",
+		srv_data.rows,
+		srv_data.cols,
+		srv_data.unavail,
+		srv_data.grps,
+		srv_data.srvs
+	);
+	return srv_data;
+}
