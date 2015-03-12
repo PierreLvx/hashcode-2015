39f070c9f7d502e373b3929e3614ca1d95ddaec1
Cedric LEGRAND <celegran@student.42.fr>
2015-03-12 19:59:48 +0100

    init all racks

--- src/init_racks.c
+++ src/init_racks.c
@@ -2,12 +2,11 @@
 
 void	init_racks(int **srv_rack, struct data *srv_data, FILE *fd)
 {
-	int 	x,y;
-	int 	i;
+	int 	x, y, i, j;
 
-	i = 0;
-	x = 0;
-	y = 0;
+	for (i = 0; i < srv_data->rows; i++)
+		for (j = 0; j < srv_data->cols; j++)
+			srv_rack[i][j] = 0;
 	while (i < srv_data->unavail)
 	{
 		fscanf(fd, "%i %i\n", &x, &y);
