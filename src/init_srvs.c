05edd07e81000eff9c55549630d7c165044f7318
Pierre Lavaux <pierre.lvx@gmail.com>
2015-03-12 19:50:05 +0100

    frappe

--- src/init_srvs.c
+++ src/init_srvs.c
@@ -6,7 +6,7 @@ struct server	*init_srvs(struct data* srv_data, FILE* fd)
 	struct server	*srvs;
 
 	srvs = malloc(sizeof(struct server) * srv_data->srvs);
-	for (i = 0; i < srv_data->srvs, i++)
+	for (i = 0; i < srv_data->srvs; i++)
 		fscanf(fd, "%i %i\n", &srvs[i].slots, &srvs[i].cap);
 	return srvs;
 }
